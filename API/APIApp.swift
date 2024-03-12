39f2d2695a59cd408bbd8af149d783d5af4f3634
Muhammad-decode <153366449+Muhammad-decode@users.noreply.github.com>
2024-03-12 15:21:08 +0500

    Initial Commit

---
+++ API/APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIApp.swift
+//  API
+//
+//  Created by Muhammad Afnan on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
